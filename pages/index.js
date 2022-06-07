cc9b8cd649393e0154753a6c60e0d2ccd1013edb
parnexcodes <parnexcodes@gmail.com>
2022-06-08 02:32:03 +0530

    ✅ change header

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Head from "next/head";
 import Footer from "../components/Footer";
 import Cards from "../components/Cards";
 
@@ -13,11 +14,16 @@ export async function getServerSideProps(context) {
 function Home({ data }) {
   return (
     <div className="min-h-screen bg-zinc-900">
+      <Head>
+        <title>OnlyCars</title>
+      </Head>
       <h1 className="text-gray-300 text-7xl text-center font-bold pt-24">
         OnlyCars
       </h1>
-      <h1 className="text-gray-300 text-center font-bold pt-4">Easily View Exotic Cars from reddit</h1>
-      <Cards data={data}/>
+      <h1 className="text-gray-300 text-center font-bold pt-4">
+        Easily View Exotic Cars from reddit
+      </h1>
+      <Cards data={data} />
       <Footer />
     </div>
   );
