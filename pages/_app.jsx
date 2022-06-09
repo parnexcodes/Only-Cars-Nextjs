0262db476aebda8c9aefea55b5047ef46ac5b20e
parnexcodes <parnexcodes@gmail.com>
2022-06-09 14:41:28 +0530

    remove chakraui

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,11 +1,8 @@
 import "../styles/globals.css";
-import { ChakraProvider } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider>
       <Component {...pageProps} />
-    </ChakraProvider>
   );
 }
 
